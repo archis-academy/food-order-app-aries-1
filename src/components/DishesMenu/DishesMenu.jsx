c4068789f2eeaad3d0be457005ff3b939b071a69
yusraylvz <128381663+yusraylvz@users.noreply.github.com>
2024-05-02 16:01:30 +0300

    src/db/foods.js oluşturuldu, dishes-menu max-width kaldırıldı, foods state i HomePage'e taşındı, propslar ile DishesMenude kullanıldı.

--- src/components/DishesMenu/DishesMenu.jsx
+++ src/components/DishesMenu/DishesMenu.jsx
@@ -1,65 +1,7 @@
 import React, { useState } from "react";
 import "./DishesMenu.scss";
 
-const DishesMenu = () => {
-  const [filter, setFilter] = useState("All");
-  const foods = [
-    {
-      id: 1,
-      image: "/Image-2.png",
-      description: "Spicy seasoned seafood noodles ",
-      price: "$ 2.29",
-      bowl: "20",
-      orderType: "Dine In",
-      type: "Hot Dishes",
-    },
-    {
-      id: 2,
-      image: "/Image-2.png",
-      description: "Salted Pasta with mushroom sauce ",
-      price: "$ 2.69",
-      bowl: "11",
-      orderType: "Dine In",
-      type: "Hot Dishes",
-    },
-    {
-      id: 3,
-      image: "/Image-3.png",
-      description: "Beef dumpling in hot and sour soup ",
-      price: "$ 2.99",
-      bowl: "16",
-      orderType: "Dine In",
-      type: "Soup",
-    },
-    {
-      id: 4,
-      image: "/Image-5.png",
-      description: "Healthy noodle with spinach leaf ",
-      price: "$ 3.29",
-      bowl: "22",
-      orderType: "To go",
-      type: "Cold Dishes",
-    },
-    {
-      id: 5,
-      image: "/Image-5.png",
-      description: "Hot spicy fried rice with omelet ",
-      price: "$ 3.49",
-      bowl: "13",
-      orderType: "Delivery",
-      type: "Hot Dishes",
-    },
-    {
-      id: 6,
-      image: "/Image-6.png",
-      description: "Spicy instant noodle with special omelette ",
-      price: "$ 3.59",
-      bowl: "17",
-      orderType: "To go",
-      type: "Hot Dishes",
-    },
-  ];
-
+const DishesMenu = ({ filter, setFilter, foods }) => {
   // Filtreleme fonksiyonu
   const filterFoods = (orderType) => {
     setFilter(orderType);
@@ -79,7 +21,11 @@ const DishesMenu = () => {
       <div className="dishes-menu-top">
         <h2 className="dishes-menu-title">Choose Dishes </h2>
 
-        <select value={filter} onChange={(e) => filterFoods(e.target.value)}>
+        <select
+          className="order-type"
+          value={filter}
+          onChange={(e) => filterFoods(e.target.value)}
+        >
           <option value="All">All</option>
           <option value="Dine In">Dine In</option>
           <option value="To go">To go</option>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,15 +2,19 @@ import Sidebar from "@/components/Sidebar/Sidebar";
 import "./HomePage.scss";
 import { useAuth } from "@/components/AuthProvider";
 import DishesMenu from "@/components/DishesMenu/DishesMenu";
+import { foods } from "@/db/foods";
+import React, { useState } from "react";
 
 function HomePage() {
   const { fireStoreUser } = useAuth(); // auth'u const {fireStoreUser} = useAuth() şeklinde alırsanız user bilgilerine ulaşabilirsiniz
+
+  const [filter, setFilter] = useState("All");
 
   return (
     <div>
       <Sidebar />
       <div className="mainRoot">
-        <DishesMenu />
+        <DishesMenu filter={filter} setFilter={setFilter} foods={foods} />
       </div>
     </div>
   );
