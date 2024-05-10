949ca7f364e9245f92a5f0af9b7d1e52892e0738
SebileGrgl <livaliva998@gmail.com>
2024-05-10 15:48:03 +0300

    initial commit

--- src/components/ProductsManagement/ProductsManagement.jsx
+++ src/components/ProductsManagement/ProductsManagement.jsx
@@ -1,9 +1,30 @@
+import ProductCard from "../ProductCard/ProductCard";
 import SettingsHeader from "../SettingsHeader/SettingsHeader";
+import { foods } from "../../db/foods";
+import "./ProductsManagement.scss";
 
 function ProductsManagement() {
   return (
     <>
-      <SettingsHeader />
+      <div className="management-container">
+        <SettingsHeader />
+        <div className="product-cards-container">
+          <div className="add-dish-card">
+            <span>+</span>
+            <p>Add new dish</p>
+          </div>
+          {foods.map((food) => {
+            return (
+              <ProductCard
+                key={food.id}
+                image={food.image}
+                description={food.description}
+                price={food.price}
+              />
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
--- src/components/SettingsHeader/SettingsHeader.jsx
+++ src/components/SettingsHeader/SettingsHeader.jsx
@@ -4,11 +4,13 @@ import "./SettingsHeader.scss";
 function SettingsHeader() {
   return (
     <>
-      <div className="title-container">
-        <h1>Products Management</h1>
-        <div className="manage-categories">
-          <img src={ManageIcon} alt="Manage-Icon" />
-          <p>Manage Categories</p>
+      <div className="header-container">
+        <div className="title-container">
+          <h1>Products Management</h1>
+          <div className="manage-categories">
+            <img src={ManageIcon} alt="Manage-Icon" />
+            <p>Manage Categories</p>
+          </div>
         </div>
       </div>
     </>
