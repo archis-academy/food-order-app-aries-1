a7a4fbb03eeddff8f825351178e31394a60c72f6
SebileGrgl <livaliva998@gmail.com>
2024-05-11 19:09:56 +0300

    [FO-12]Html css parts are completed

--- src/components/ProductsManagement/ProductsManagement.jsx
+++ src/components/ProductsManagement/ProductsManagement.jsx
@@ -2,18 +2,37 @@ import ProductCard from "../ProductCard/ProductCard";
 import SettingsHeader from "../SettingsHeader/SettingsHeader";
 import { foods } from "../../db/foods";
 import "./ProductsManagement.scss";
+import CategoryTabs from "../CategoryTabs/CategoryTabs";
+import { useState } from "react";
+import ChangeButtons from "../ChangeButtons/ChangeButtons";
 
 function ProductsManagement() {
+  const [dishes, setDishes] = useState(foods);
+  const [filteredDishes, setFilteredDishes] = useState(dishes);
+  const [filterParameters, setFilterParameters] = useState({
+    orderType: "All",
+    category: "all",
+    searchQuery: "",
+  });
+
   return (
     <>
       <div className="management-container">
-        <SettingsHeader />
+        <div className="header-sticky">
+          <SettingsHeader />
+          <CategoryTabs
+            dishes={dishes}
+            setFilteredDishes={setFilteredDishes}
+            filterParameters={filterParameters}
+            setFilterParameters={setFilterParameters}
+          />
+        </div>
         <div className="product-cards-container">
           <div className="add-dish-card">
             <span>+</span>
             <p>Add new dish</p>
           </div>
-          {foods.map((food) => {
+          {filteredDishes.map((food) => {
             return (
               <ProductCard
                 key={food.id}
@@ -24,6 +43,7 @@ function ProductsManagement() {
             );
           })}
         </div>
+        <ChangeButtons />
       </div>
     </>
   );
