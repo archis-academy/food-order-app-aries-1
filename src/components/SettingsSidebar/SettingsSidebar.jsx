2cd7b08aa72b3efe46bdf815538c44a900bc2244
Alperen Üstün <alperen.ustun39@gmail.com>
2024-05-21 16:59:27 +0300

    [FO-11] chore: Update NavLink styling in SettingsSidebar.jsx

--- src/components/SettingsSidebar/SettingsSidebar.jsx
+++ src/components/SettingsSidebar/SettingsSidebar.jsx
@@ -58,7 +58,13 @@ function SettingsSidebar() {
   return (
     <div className="sidebar-container">
       {tabs.map((tab) => (
-        <NavLink key={tab.id} to={tab.path}>
+        <NavLink
+          key={tab.id}
+          to={tab.path}
+          className={({ isActive }) => {
+            return isActive && "active";
+          }}
+        >
           <div className="list-group">
             {tab.icon}
             <div className="list-title-box">
