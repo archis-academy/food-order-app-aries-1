1ed71d93589806ada04ac6fb442d6884fb859eca
yusraylvz <128381663+yusraylvz@users.noreply.github.com>
2024-05-22 18:05:04 +0300

    FoodCard tek bir 'food' prop'u alacak şekilde güncellendi ve DishesMenu'ye taşındı

--- src/components/DishesMenu/FoodCard/FoodCard.jsx
+++ src/components/DishesMenu/FoodCard/FoodCard.jsx
@@ -1,8 +1,9 @@
 import "./FoodCard.scss";
 
-const FoodCard = ({ id, image, description, price, bowl, onClick }) => {
+const FoodCard = ({ food, onClick }) => {
+  const { image, description, price, bowl } = food;
   return (
-    <div className="food-card" key={id} onClick={onClick}>
+    <div className="food-card" onClick={onClick}>
       <img className="food-image" src={image} />
       <p className="food-description"> {description}</p>
       <p className="food-price">{price}</p>
