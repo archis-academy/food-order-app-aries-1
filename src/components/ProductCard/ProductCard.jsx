b22e4bcd9dec616cf4339acf0a6cb43f57e11849
SebileGrgl <livaliva998@gmail.com>
2024-05-28 15:17:30 +0300

    [FO-11]edited classnames

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,12 +4,12 @@ function ProductCard({ image, description, price }) {
   return (
     <>
       <div className="product-card">
-        <img className="food-image" src={image} alt="product-image" />
-        <div className="details">
+        <img className="product-card-image" src={image} alt="product-image" />
+        <div className="product-card-details">
           <p>{description}</p>
           <span>{price}</span>
         </div>
-        <div className="edit-box">
+        <div className="product-card-edit-box">
           <img src={EditIcon} alt="edit-icon" />
           <p>Edit Dish</p>
         </div>
