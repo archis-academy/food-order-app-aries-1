d22ce34f1d9998ef52ad97e31c478bd9158b6ea7
yusraylvz <128381663+yusraylvz@users.noreply.github.com>
2024-06-25 13:20:59 +0300

    Login page redesign yapıldı

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -37,26 +37,46 @@ function LoginPage() {
   }
 
   return (
-    <div>
-      <h1>Login page</h1>
-      <form onSubmit={(e) => handleUserLogin(e)}>
-        <input
-          onKeyUp={(e) => handleChange(e)}
-          name="password"
-          type="password"
-        />
-        <input onKeyUp={(e) => handleChange(e)} name="email" type="email" />
-        <input type="submit" value="Login" />
-      </form>
+    <div className="login-page">
+      <div className="login-container">
+        <h1 className="login-title">Login</h1>
+        <form onSubmit={(e) => handleUserLogin(e)}>
+          <div className="login-email-container">
+            <img className="login-icon" src="/person-icon.svg"></img>
+            <input
+              className="login-email"
+              onKeyUp={(e) => handleChange(e)}
+              name="email"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="login-password-container">
+            <img className="login-icon" src="/lock-icon.svg"></img>
+            <input
+              className="login-password"
+              onKeyUp={(e) => handleChange(e)}
+              name="password"
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <input className="login-btn" type="submit" value="Login" />
 
-      <button
+          <a className="login-signup" href="#">
+            Sign Up
+          </a>
+        </form>
+
+        {/* <button
         onClick={() => {
           signOut(auth);
           console.log("clicked");
-        }}
-      >
-        Logout
-      </button>
+          }}
+          >
+          Logout
+          </button> */}
+      </div>
     </div>
   );
 }
